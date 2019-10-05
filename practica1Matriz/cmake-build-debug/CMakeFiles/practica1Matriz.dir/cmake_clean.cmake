b324589d5bf80f96d64abf6efa23ae6e79c1ddda
axxa <asan123456@gmail.com>
2019-10-05 03:20:25 +0200

    feat: first commit

---
+++ practica1Matriz/cmake-build-debug/CMakeFiles/practica1Matriz.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practica1Matriz.dir/main.cpp.o"
+  "CMakeFiles/practica1Matriz.dir/vector.cpp.o"
+  "practica1Matriz"
+  "practica1Matriz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practica1Matriz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ practica1Matriz/main.cpp
@@ -0,0 +1,8 @@
+#include "vector.h"
+#include <iostream>
+
+int main() {
+    vector v{5};
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
