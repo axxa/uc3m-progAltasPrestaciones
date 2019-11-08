8b77d56f1b3655329b8445e4271699f5172cbdd4
axxa <asan123456@gmail.com>
2019-11-08 14:00:29 +0100

    feat: template for type of matrix

--- practica2MatrizGenerica/main.cpp
+++ practica2MatrizGenerica/main.cpp
@@ -1,27 +1,35 @@
 #include "matrix.h"
+#include "racional.h"
 #include <iostream>
 #include <random>
 #include <chrono>
 using namespace std;
 
-int n = 1000;
+int n = 10;
 
-void evaluacion(){
-    matrix a{n,n};
-    matrix b{n,n};
-    matrix c{n,n};
-
-    double s = 0;
+void evaluacionRacional(){
+    matrix<racional<int>> a{n,n};
+    matrix<racional<int>> b{n,n};
+    matrix<racional<int>> c{n,n};
+    matrix<racional<int>> d{n,n};
+    racional<int> s{0,0};
 
     std::random_device rd{};
     std::mt19937 gen{rd()};
     std::normal_distribution<> dis(10, 2.5);
+
     for (int i = 0; i < a.getSize(); ++i) {
-        a.set( i , dis(gen) );
-        b.set( i , dis(gen) );
-        c.set( i , dis(gen) );
+
+        racional<int> r{static_cast<int>(dis(gen)),static_cast<int>(dis(gen))};
+        //a.set( i , dis(gen) );
+        //b.set( i , dis(gen) );
+        //c.set( i , dis(gen) );
+
+        a.set( i , r );
+        b.set( i , r );
+        c.set( i , r );
     }
-    matrix d{n,n};
+
     auto start = chrono::high_resolution_clock::now();
     d = a + b * c;
     auto end = chrono::high_resolution_clock::now();
@@ -39,17 +47,61 @@ void evaluacion(){
 
 }
 
-void testMovimientoYCopia(){
-    matrix b{1,1}; //por defecto
-    b.set(0,2);
+void evaluacion(){
 
-    matrix a{b}; // copia
+    matrix<float> a{n,n};
+    matrix<float> b{n,n};
+    matrix<float> c{n,n};
+    matrix<float> d{n,n};
+    float s = 0;
 
-    matrix c{std::move(b)}; //movimiento
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<> dis(10, 2.5);
+    for (int i = 0; i < a.getSize(); ++i) {
+        a.set( i , dis(gen) );
+        b.set( i , dis(gen) );
+        c.set( i , dis(gen) );
+    }
+
+    auto start = chrono::high_resolution_clock::now();
+    d = a + b * c;
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> diff = end-start;
+
+    cout << "Tiempo de cálculo de la matriz D = " << diff.count() << endl;
+
+    for (int i = 0; i < d.getSize(); ++i) {
+        s = s + d.get(i);
+    }
+
+    s = s/(d.getSize());
+
+    cout << "\n" << "s = " << s << "\n";
+
+}
+
+void testracional(){
+    racional<int> r{1,3};
+    racional<int> r2{1,6};
+
+    cout << r+ r2;
+
+    racional<int> r3{1,3};
+    racional<int> r4{1,5};
+
+    cout << r3+ r4;
+
+    racional<int> r5{2,5};
+    racional<int> r6{3,4};
+
+    cout << r5 *  r6;
+
 }
 int main() {
-    evaluacion();
-    //testMovimientoYCopia();
+    //evaluacion();
+    evaluacionRacional();
+    //testracional();
     return 0;
 }
 
--- practica2MatrizGenerica/matrix.h
+++ practica2MatrizGenerica/matrix.h
@@ -6,6 +6,7 @@
 #define MATRIX_H
 #include <iostream>
 
+template <typename T>
 class matrix {
     public:
 
@@ -40,7 +41,7 @@ class matrix {
         //-------------------------------------------------------
         //5. El operador paréntesis se sobrecargará con dos argumentos para acceder a una
         //posición de la matriz
-        double operator()(int i, int j) const { return buffer_[i * cols_ + j]; }
+        T operator()(int i, int j) const { return buffer_[i * cols_ + j]; }
         //-------------------------------------------------------
         //6. Los operadores + y * se sobrecargarán para implementar la suma y el producto de
         //matrices.
@@ -53,22 +54,171 @@ class matrix {
         int getCols() const;
         int getSize() const;
 
-        double * getBuffer() const;
-        double * setBuffer();
-        double get(int i, int j) const { return buffer_[i * cols_ + j]; }
-        double get(int pos) const { return buffer_[pos]; }
-        void set(int i, int j, double x) { buffer_[i * cols_ + j] = x; }
-        void set(int pos, double x) { buffer_[pos] = x; }
+        T * getBuffer() const;
+        T * setBuffer();
+        T get(int i, int j) const { return buffer_[i * cols_ + j]; }
+        T get(int pos) const { return buffer_[pos]; }
+        void set(int i, int j, T x) { buffer_[i * cols_ + j] = x; }
+        void set(int pos, T x) { buffer_[pos] = x; }
 
     private:
         int rows_;
         int cols_;
-        double * buffer_;
+        T * buffer_;
 
 
 };
 
-std::ostream & operator<<(std::ostream & fs, const matrix & v);
-std::istream & operator>>(std::istream & fe, matrix & v);
+template <typename T>
+std::ostream & operator<<(std::ostream & fs, const matrix<T> & v);
+template <typename T>
+std::istream & operator>>(std::istream & fe, matrix<T> & v);
+
+
+
+template <typename T>
+matrix<T>::matrix(int i, int j) :
+        rows_{(i>0)?(i):0},
+        cols_{(j>0)?(j):0},
+        buffer_{((i==0) && (j==0))?(nullptr):new T[i*j]{}}  // Inicia todos a 0.0
+{
+    std::cout<< "CONSTRUCTOR POR DEFECTO" << "\n";
+}
+
+template <typename T>
+matrix<T>::matrix(const matrix<T> & v)
+{
+    std::cout<< "CONSTRUCTOR DE COPIA" << "\n";
+
+    T * aux = new T[v.getSize()];
+    std::copy(v.buffer_, v.buffer_+v.getSize(), aux);
+    delete [] buffer_;
+    rows_ = v.getRows();
+    cols_ = v.getCols();
+    buffer_ = aux;
+
+}
+
+template <typename T>
+int matrix<T>::getRows() const {
+    return rows_;
+}
+
+template <typename T>
+int matrix<T>::getCols() const {
+    return cols_;
+}
+
+template <typename T>
+int matrix<T>::getSize() const {
+    return rows_*cols_;
+}
+
+template <typename T>
+T * matrix<T>::getBuffer() const {
+    return buffer_;
+}
+
+template <typename T>
+matrix<T> & matrix<T>::operator=(const matrix<T> & v)
+{
+    std::cout<< "OPERADOR DE COPIA" << "\n";
+    T * aux = new T[v.getSize()];
+    std::copy(v.buffer_, v.buffer_+v.getSize(), aux);
+    delete [] buffer_;
+    rows_ = v.getRows();
+    cols_ = v.getCols();
+    buffer_ = aux;
+
+    return *this;
+}
+
+template <typename T>
+matrix<T> & matrix<T>::operator=(const matrix<T> && m)
+{
+    std::cout<< "OPERADOR DE MOVIMIENTO" << "\n";
+    if (this != &m)
+    {
+        delete [] buffer_;
+        rows_ = m.rows_;
+        cols_ = m.cols_;
+        buffer_ = m.buffer_;
+
+    }
+
+    return *this;
+}
+
+template <typename T>
+matrix<T> & matrix<T>::operator+(const matrix<T> & m)
+{
+    if(rows_ == m.getRows() && cols_ == m.getCols())
+        for(int i = 0 ; i < m.getSize() ; i++){
+            this->buffer_[i] = this->buffer_[i] + m.getBuffer()[i];
+        }
+    return *this;
+
+}
+
+template <typename T>
+matrix<T> & matrix<T>::operator*(const matrix<T> & m)
+{
+    if(cols_ == m.getRows()) {
+        matrix<T> aux{rows_,m.getCols()};
+        int z = 0;
+        int zIni = 0;
+        int k=0;
+
+        for (int i = 0; i < getSize(); i++) {
+            zIni = k * aux.cols_;
+            z = zIni;
+
+            if((i-1) % aux.cols_ == 0){
+                z = zIni;
+                i = z;
+            }
+
+            for (int j = 0; j < m.getSize(); j++) {
+                if(j==0)k++;
+                aux.set(z, aux.get(z) + get(i) * m.get(j));
+                z++;
+
+                if((j+1) % aux.cols_ == 0 ){
+                    i++;
+                    z = zIni;
+                }
+            }
+        }
+        *this = aux;
+    }
+
+    return *this;
+
+}
+
+template <typename T>
+std::ostream & operator<<(std::ostream & fs, const matrix<T> & m) {
+    for (int i=0; i<m.getRows(); ++i) {
+        fs << "\n" ;
+        for (int j=0; j<m.getCols(); ++j) {
+            fs << m.get(i, j) << " ";
+        }
+    }
+    return fs;
+}
+
+template <typename T>
+std::istream & operator>>(std::istream & fe, matrix<T> & m) {
+    int i=0;
+    int j=0;
+    T x;
+    while ((i+j<m.getSize() ) && (fe >> x)) {
+        if(j >= m.getCols()){
+            i++;
+        }
+        m.set(i,j++, x);
+    }
+    return fe;
+}
 
 #endif //MATRIX_H
